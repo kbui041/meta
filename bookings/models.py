f6f3a5cdacc9bbdf8fda56bcd0339759de5daf7e
kbui041 <kbui04@uw.edu>
2024-08-02 12:07:16 +0700

    current

--- bookings/models.py
+++ bookings/models.py
@@ -2,6 +2,7 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 # Model for categories
 class Category(models.Model):
